d57c9847296c57a0e3eaa111c820f7099287a0f5
hooriza <hooriza.com@linecorp.com>
2022-03-29 00:08:28 +0900

    Scoped slots

--- src/module/useSlot.ts
+++ src/module/useSlot.ts
@@ -3,7 +3,7 @@ import { Children } from "react";
 
 const useSlot = (children: ReactNode) => {
   const slots = Children.toArray(children).reduce<
-    Record<string, ReturnType<typeof Children.toArray>>
+    Partial<Record<string, ReturnType<typeof Children.toArray>>>
   >((slots, child) => {
     if (typeof child === "object" && "props" in child && child.props.slot) {
       const slotName = child.props.slot;
@@ -22,8 +22,26 @@ const useSlot = (children: ReactNode) => {
     };
   }, {});
 
-  return ({ name = "default", children }: { name?: string, children?: ReactNode }) =>
-    createElement(Fragment, {}, slots[name] ?? children);
+  return ({
+    name = "default",
+    data,
+    children
+  }: {
+    name?: string;
+    data?: unknown;
+    children?: ReactNode;
+  }) => {
+    let slot = slots[name];
+
+    if (data && slot) {
+      slot = slot.map((child) =>
+        typeof child === "object" && "props" in child
+          ? cloneElement(child, {}, child.props.children(data))
+          : child
+      );
+    }
+    return createElement(Fragment, {}, slot ?? children);
+  };
 };
 
 export default useSlot;
