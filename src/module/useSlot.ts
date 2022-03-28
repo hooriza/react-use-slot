bedf3c7d7899164138ce3e114d76f128c7320f55
hooriza <hooriza.com@linecorp.com>
2022-03-29 01:07:12 +0900

    Refactor code

--- src/module/useSlot.ts
+++ src/module/useSlot.ts
@@ -1,6 +1,32 @@
 import { createElement, cloneElement, Fragment, ReactNode } from "react";
 import { Children } from "react";
 
+interface SlotProps {
+  name?: string;
+  data?: unknown;
+  children?: ReactNode;
+}
+
+const createSlot = (
+  slots: Partial<Record<string, ReturnType<typeof Children.toArray>>>
+) => ({
+  name = "default",
+  data,
+  children
+}: SlotProps) => {
+  let slot = slots[name];
+
+  if (data && slot) {
+    slot = slot.map((child) =>
+      typeof child === "object" && "props" in child
+        ? cloneElement(child, {}, child.props.children(data))
+        : child
+    );
+  }
+
+  return createElement(Fragment, {}, slot ?? children);
+};
+
 const useSlot = (children: ReactNode) => {
   const slots = Children.toArray(children).reduce<
     Partial<Record<string, ReturnType<typeof Children.toArray>>>
@@ -22,26 +48,7 @@ const useSlot = (children: ReactNode) => {
     };
   }, {});
 
-  return ({
-    name = "default",
-    data,
-    children
-  }: {
-    name?: string;
-    data?: unknown;
-    children?: ReactNode;
-  }) => {
-    let slot = slots[name];
-
-    if (data && slot) {
-      slot = slot.map((child) =>
-        typeof child === "object" && "props" in child
-          ? cloneElement(child, {}, child.props.children(data))
-          : child
-      );
-    }
-    return createElement(Fragment, {}, slot ?? children);
-  };
+  return createSlot(slots);
 };
 
 export default useSlot;
