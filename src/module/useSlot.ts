a18cbcc863af01e11d3a934e378a2f36d0a9f944
hooriza <hooriza.com@linecorp.com>
2022-03-29 08:56:40 +0900

    Occurs error if there is not expected children

--- src/module/useSlot.ts
+++ src/module/useSlot.ts
@@ -17,11 +17,16 @@ const createSlot = (
   let slot = slots[name];
 
   if (typeof data !== 'undefined' && slot) {
-    slot = slot.map((child) =>
-      typeof child === "object" && "props" in child
-        ? cloneElement(child, {}, child.props.children(data))
-        : child
-    );
+
+    slot = slot.map((child) => {
+      if (typeof child === "object" && "props" in child) {
+        if (process.env.NODE_ENV === 'development' && typeof child.props.children !== 'function') {
+          throw new Error(`The children of a component using "${name}" slot must be a function`);
+        }
+        return cloneElement(child, {}, child.props.children(data));
+      }
+      return child;
+    });
   }
 
   return createElement(Fragment, {}, slot ?? children);
